7d2a46e3c41b34a55f465af1ef0d16636d896072
Gleb Lysov <lysovg9@gmail.com>
2020-10-05 20:54:12 +0300

    lol

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+template <int N> struct A
+{
+  int a[N];
+  int b[N];
+};
+
 int main() {
-    std::cout<<"Hello, world"<<"\n";
+    std::cout<<"Hello, world"<<'\n';
+    auto arr = A<2> {1, 2, 3, 4};
+    std::cout<<arr.b[0]<<' '<<arr.a[1]<<'\n';
 }
