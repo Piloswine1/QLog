89f0c1884e104b617748bc19d55a533e8b47b2b2
Gleb Lysov <lysovg9@gmail.com>
2020-10-05 20:46:23 +0300

    some boilerplate

---
+++ tests/test3.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+TEST(simple, failing_assert)
+{
+    ASSERT_EQ(1, 0) << "1 is not equal 0";
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
